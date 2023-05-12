4d9de9cd9351f04c97dd3f28645ffde410432013
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2023-05-12 16:42:05 -0300

    configuring and creating an app in storybook

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,8 +1,25 @@
 "use strict";
 var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -30,50 +47,44 @@ var colors = {
   black: "#000000",
   linkActive: "#1A0DAB",
   linkVisited: "#681DA8",
-  primary: {
-    blue50: "#E3F2FD",
-    blue100: "#BBDEFB",
-    blue200: "#90CAF9",
-    blue300: "#64B5F6",
-    blue400: "#42A5F5 ",
-    blue500: "#2196F3",
-    blue600: "#1E88E5 ",
-    blue700: "#1976D2",
-    blue800: "#1565C0",
-    blue900: "#1565C0",
-    A100: "#82B1FF",
-    A200: "#448AFF",
-    A400: "#2979FF",
-    A700: "#2962FF"
-  },
-  secondaryColor: {
-    beige50: "#FCFAF6",
-    beige100: "#F9F4EC",
-    beige200: "#F2EAD9",
-    beige300: "#E9DABD",
-    beige400: "#DAC7A1",
-    beige500: "#B2A180",
-    beige600: "#9C8D70",
-    beige700: "#867960",
-    beige800: "#6F6550",
-    beige900: "#413C30"
-  },
-  tertiaryColor: {
-    amber50: "#FFF8E1",
-    amber100: "#FFECB3",
-    amber200: "#FFE082",
-    amber300: "#FFD54F",
-    amber400: "#FFCA28",
-    amber500: "#FFC107",
-    amber600: "#FFB300",
-    amber700: "#FFA000",
-    amber800: "#FF8F00",
-    amber900: "#FF6F00",
-    A100: "#FFE57F",
-    A200: "#FFD740",
-    A400: "#FFC400",
-    A700: "#FFAB00"
-  },
+  blue50: "#E3F2FD",
+  blue100: "#BBDEFB",
+  blue200: "#90CAF9",
+  blue300: "#64B5F6",
+  blue400: "#42A5F5 ",
+  blue500: "#2196F3",
+  blue600: "#1E88E5 ",
+  blue700: "#1976D2",
+  blue800: "#1565C0",
+  blue900: "#1565C0",
+  blueA100: "#82B1FF",
+  blueA200: "#448AFF",
+  blueA400: "#2979FF",
+  blueA700: "#2962FF",
+  beige50: "#FCFAF6",
+  beige100: "#F9F4EC",
+  beige200: "#F2EAD9",
+  beige300: "#E9DABD",
+  beige400: "#DAC7A1",
+  beige500: "#B2A180",
+  beige600: "#9C8D70",
+  beige700: "#867960",
+  beige800: "#6F6550",
+  beige900: "#413C30",
+  amber50: "#FFF8E1",
+  amber100: "#FFECB3",
+  amber200: "#FFE082",
+  amber300: "#FFD54F",
+  amber400: "#FFCA28",
+  amber500: "#FFC107",
+  amber600: "#FFB300",
+  amber700: "#FFA000",
+  amber800: "#FF8F00",
+  amber900: "#FF6F00",
+  amberA100: "#FFE57F",
+  amberA200: "#FFD740",
+  amberA400: "#FFC400",
+  amberA700: "#FFAB00",
   grayRiver50: "#EBEEF3",
   grayRiver100: "#CED5DE",
   grayRiver200: "#B0B9C6",
@@ -115,11 +126,110 @@ var colors = {
   error800: "#FC3117",
   error900: "#C80000"
 };
+var space = {
+  none: "0",
+  xs: "0.25rem",
+  sm: "0.5rem",
+  md: "1rem",
+  lg: "1.5rem",
+  xl: "2rem",
+  "2xl": "3rem",
+  "3xl": "4rem",
+  "4xl": "6.5rem",
+  "5xl": "7.5rem"
+};
+var media = {
+  xs: "(min-width: 320px)",
+  sm: "(min-width: 375px)",
+  md: "(min-width: 640px)",
+  lg: "(min-width: 1024px)",
+  xl: "(min-width: 1260px)"
+};
+var radii = {
+  px: "1px",
+  xs: "4px",
+  sm: "6px",
+  md: "8px",
+  lg: "16px",
+  full: "99999px"
+};
+var fonts = {
+  default: "Segoe UI, sans-serif",
+  code: "monospace"
+};
+var fontSizes = {
+  xxs: "0.625rem",
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.25rem",
+  "6xl": "3rem",
+  "7xl": "4rem",
+  "8xl": "4.5rem",
+  "9xl": "6rem"
+};
+var fontWeights = {
+  regular: "400",
+  medium: "500",
+  bold: "700"
+};
+var lineHeights = {
+  shorter: "125%",
+  short: "140%",
+  base: "160%",
+  tall: "180%"
+};
+var shadows = {
+  shadow: "-4px -4px 10px 0px #000000, 4px 4px 6px 0px #000000;",
+  "shadow-md": "4px 3px 20px 4px #000000, 2px 2px 4px 0px #000000, 4px 4px 10px 0px #000000",
+  "shadow-lg": "0rem 0.5rem 1rem rgba(0, 0, 0, 0.15)",
+  "shadow-xl": "0rem 1rem 1rem 0rem rgba(0, 0, 0, 0.15)"
+};
+
+// src/styles/index.ts
+var import_react = require("@stitches/react");
+var {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config
+} = (0, import_react.createStitches)({
+  themeMap: __spreadProps(__spreadValues({}, import_react.defaultThemeMap), {
+    height: "space",
+    width: "space"
+  }),
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+    shadows,
+    media
+  }
+});
 
 // src/index.tsx
 var import_jsx_runtime = require("react/jsx-runtime");
+var Button = styled("button", {
+  fontFamily: "$default",
+  backgroundColor: "$blueA100",
+  borderRadius: "$md",
+  padding: "$2xl",
+  width: "$2xl"
+});
 function App() {
-  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)("h1", { style: { color: colors.secondaryColor.beige50 }, children: "Ol\xE1 mundo!" });
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(Button, { children: "Ol\xE1 mundo!" });
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
