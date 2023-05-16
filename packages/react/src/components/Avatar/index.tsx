705e75692c1acfe8750b7744f2f84394daaacd83
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2023-05-16 11:32:13 -0300

    finished avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from "react"
+import { AvatarContainer, AvatarFallback, AvatarImage } from "./styles"
+import { UserIcon } from '@heroicons/react/24/solid'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+
+export function Avatar(props: AvatarProps) {
+    return (
+        <AvatarContainer>
+            <AvatarImage 
+                {...props} 
+            />
+
+            <AvatarFallback delayMs={600}>
+                <UserIcon />
+            </AvatarFallback>
+        </AvatarContainer>
+    )
+}
