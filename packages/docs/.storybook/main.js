9f3c3a21cae1b35970a2677b6976c73b6a2c1cae
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2023-05-18 10:20:54 -0300

    fix: Add subpath to Storybook on  production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/LibReact/'
+    }
+    return config
+  }
 };
 export default config;
